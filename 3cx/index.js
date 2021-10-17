88eff448e3d59794527b4165ab9f21404094732a
Luis Zampieron <luisfernandozampieron@gmail.com>
2021-10-17 15:22:56 -0300

    Refactoring alterando o selenium pelo puppeteer

--- 3cx/index.js
+++ 3cx/index.js
@@ -1,95 +1,21 @@
-const { Builder, By, Key, until, element } = require('selenium-webdriver')
+const puppeteer = require('puppeteer');
 const fetch = require('cross-fetch')
 const configs = require('../configs')
 
 
 // Retorna um array de objetos que mostram a quantidade de ligação de todos os técnicos
-async function queue_agents(driver) {
-    const arr = []
+async function queue_agents(page) {
 
-    const elements = await driver.findElements(By.css("ul"));
-    const element = await elements[1].getText()
-    const arrayElement = element.split('\n')
-
-    // Remove as informações de Login e as iniciais da foto de perfil
-    for (let i = 0; i < arrayElement.length; i++) {
-        if (arrayElement[i].length == 2 || arrayElement[i].slice(0, 1) == "Q") {
-            arrayElement.splice(i, 1) 
-            i = 0
-        }
-    }
-
-    // Remove os técnicos que não tiveram ligações
-    for (let i = 0; i < arrayElement.length; i++) {
-        if (arrayElement[i].slice(0, 1) == "0") {
-            arrayElement.splice(i - 1, 2)
-            i = 0
-        }
-    }
-
-    // Adiciona as informações no Array
-    for (let i = 0; i < arrayElement.length; i += 2) {
-        obj = {
-            user: arrayElement[i],
-            serviced_abandoned_time: arrayElement[i + 1]
-        }
-        arr.push(obj)
-    }
-
-    return arr
 }
 
 // Retorna um objeto com status gerais
-async function queue_stats(driver) {
-    const findElementStats = await driver.findElements(By.css("queue-stat"))
-    const elements = await findElementStats[0].getText()
-    const element = elements.split('\n')
-    const arrayElement = element[1].split(' ')
+async function queue_stats(page) {
 
-    obj = {
-        "waiting": arrayElement[0],
-        "serviced": arrayElement[1],
-        "abandoned": arrayElement[2],
-        "longest_waiting": arrayElement[3],
-        "average_waiting_time": arrayElement[4],
-        "average_talking_time": arrayElement[5],
-    }
-
-    return obj
 }
 
 // Retorna um array de objetos com os tecnicos que estão em ligação
-async function active_calls(driver) {
-    const arr = []
+async function active_calls(page) {
 
-    const elements = await driver.findElements(By.css("active-calls"))
-    const element = await elements[0].getText()
-    const arrayElement = element.split('\n')
-
-    // Remove valores não necessários (ex: detalhes, fila suporte)
-    arrayElement.splice(0, 5)
-
-    // Remove as informações de Chamada externa/External Call
-    for (let i = 0; i < arrayElement.length; i++) {
-        // Feito essas duas verificações pois ela muda de acordo com o idioma
-        if (arrayElement[i].slice(0, 15) == "Chamada externa" || 
-            arrayElement[i].slice(0, 13) == "External Call") {
-            arrayElement.splice(i, 1)
-            i = 0
-        }
-    }
-
-     // Adiciona as informações no Array
-    for (let i = 0; i < arrayElement.length; i += 3) {
-        obj = {
-            user: arrayElement[i + 1],
-            phone: arrayElement[i],
-            time_called: arrayElement[i + 2]
-        }
-        arr.push(obj)
-    }
-
-    return arr
 }
 
 // Faz o post na api
@@ -110,11 +36,11 @@ async function connectionApi(data) {
 
 
 // Cria o JSON que será feito o post e já chama a conexão com a api
-async function CreateJSON(driver) {
+async function CreateJSON(page) {
     try {
-        data_active_calls = await active_calls(driver)
-        data_queue_agents = await queue_agents(driver)
-        data_queue_stats = await queue_stats(driver)
+        data_active_calls = await active_calls(page)
+        data_queue_agents = await queue_agents(page)
+        data_queue_stats = await queue_stats(page)
 
         data = {
             "active_calls": data_active_calls,
@@ -129,21 +55,26 @@ async function CreateJSON(driver) {
 }
 
 // Function main... Abre o navegador e chama as demais funções
-async function get_data() {
-    let driver = await new Builder().forBrowser('firefox').build()
+(async () => {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
 
-    await driver.get(configs.urlLogin)
-    await driver.findElement(By.id('loginInput')).sendKeys(configs.user)
-    await driver.findElement(By.id('passwordInput')).sendKeys(configs.password, Key.RETURN)
+    // - Acessa página de login
+    await page.goto(configs.urlLogin)
+
+    // - Loga na página
+    await page.type('#loginInput', configs.user)
+    await page.type('#passwordInput', configs.password)
+    await page.click('#submitBtn')
 
 
-    await driver.switchTo().newWindow('tab');
-    await driver.get(configs.urlWallboard);
+    // - Acessa página de wallboard
+    await page.waitForTimeout(3000)
+    await page.goto(configs.urlWallboard)
 
+    await page.waitForTimeout(3000)
 
     setInterval(() => {
-        CreateJSON(driver)
+        CreateJSON(page)
     }, 5000)
-}
-
-get_data()
+})()
